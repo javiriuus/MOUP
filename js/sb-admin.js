0ed2f08bd90a336e1ee29d15829e03123a822671
Mario <casterlyrock17@gmail.com>
2019-12-01 12:49:59 +0100

    Pequeña corrección del Script del editor

--- js/sb-admin.js
+++ js/sb-admin.js
@@ -1,3 +1,27 @@
+window.eh = ace.edit("editor_python");
+eh.setTheme("ace/theme/cobalt");
+eh.session.setMode("ace/mode/python");
+
+window.ec = ace.edit("editor_js");
+ec.setTheme("ace/theme/cobalt");
+ec.session.setMode("ace/mode/javascript");
+
+window.eh = ace.edit("editor_ruby");
+eh.setTheme("ace/theme/cobalt");
+eh.session.setMode("ace/mode/ruby");
+
+window.eh = ace.edit("editor_java");
+eh.setTheme("ace/theme/cobalt");
+eh.session.setMode("ace/mode/java");
+
+window.eh = ace.edit("editor_css");
+eh.setTheme("ace/theme/cobalt");
+eh.session.setMode("ace/mode/css");
+
+window.eh = ace.edit("editor_swift");
+eh.setTheme("ace/theme/cobalt");
+eh.session.setMode("ace/mode/swift");
+
 (function($) {
   "use strict"; // Start of use strict
 
